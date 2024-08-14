9672442c45b7d746d8f41a6c8b1496906051a319
kursatabayli <krst.abayli@outlook.com>
2024-08-14 16:48:14 +0300

    Add project files.

---
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,50 @@
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7278/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
+                return View(values);
+            }
+            return View(); 
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7278/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultGetBrandsDto>>(jsonData);
+            List<SelectListItem> BrandValues = (from x in values 
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value=x.BrandID.ToString(),
+                                                }).ToList();
+            ViewBag.BrandValues = BrandValues;
+
+            return View();
+        }
+    }
+}
